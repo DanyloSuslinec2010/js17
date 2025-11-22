83991be5095535c05e02e1a325846af6734762d3
DanyloSuslinec2010 <danickasuslinec@gmail.com>
2025-11-22 15:26:21 +0100

    asdasd

--- index.js
+++ index.js
@@ -1,58 +1,24 @@
-const input = document.getElementById("textInput");
-const button = document.getElementById("changeBtn");
+const input = document.querySelector("#text-input");
+const button = document.querySelector("#change-btn");
+
+button.textContent = input.value; 
+
 
 button.addEventListener("click", () => {
-  if (input.value.trim() !== "") {
-    button.textContent = input.value;
-  }
+  button.textContent = input.value;
 });
 
+const img = document.querySelector('#my-image');
+
+img.src = 'https://picsum.photos/400/300';
 
 
+const link = document.querySelector("#my-link");
+const image = document.querySelector("#link-image");
 
-const image = document.getElementById("myImage");
-image.src =
-  "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&w=400";
+link.href = "https://google.com";
+image.alt = "Нове описання зображення";
 
+const firstItem = document.querySelector("#my-list li");
 
-
-const link = document.getElementById("myLink");
-const linkImg = document.getElementById("linkImage");
-
-link.href = "https://openai.com";
-linkImg.alt = "Гарне зображення коней";
-
-
-
-
-
-
-
-
-
-const list = document.getElementById("myList");
-const firstItem = list.querySelector("li");
 firstItem.textContent = "Новий текст для першого елемента";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
